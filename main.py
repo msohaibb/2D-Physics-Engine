e99efbaaf48c769122d9d80eae751eaab06cedb7
msohaibb <44452143+msohaibb@users.noreply.github.com>
2019-05-09 14:54:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import math
+from time import sleep
+
+g = -9.8
+time = 0
+
+
+def main(initial_v, theta, height):
+    initial_y = math.sin(math.radians(theta)) * initial_v
+    initial_x = math.cos(math.radians(theta)) * initial_v
+    seconds = 0
+    while height > 0:
+        height = find_delta_y(initial_y, seconds, height)
+        x_displacement = find_delta_x(initial_x, seconds)
+        print("At t = " + str(seconds) + " seconds, the x value is " + str(x_displacement) + ", and the y value is " + str(height) + ".")
+        seconds += 1
+        sleep(1)
+
+
+def find_delta_y(initial_y, sec, height):
+    return initial_y * sec + (g / 2) * math.pow(sec, 2) + height
+
+
+def find_delta_x(initial_x, sec):
+    return initial_x * sec
+
+
+main(30, 30, 30)
+
